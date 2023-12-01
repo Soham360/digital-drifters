21d7a6fd6c34ef12ea29bf4be4425bcbe219d6cd
Aniket Chakradeo <91163802+AniCricKet@users.noreply.github.com>
2023-12-01 11:50:35 -0800

    Create Sort.java

---
+++ src/main/java/com/nighthawk/spring_portfolio/Sort.java
@@ -0,0 +1,17 @@
+public abstract class Sort {
+    int[] data;
+    int iterations;
+    int comparisons;
+    int swaps;
+
+    public abstract void init(int[] data);
+
+    public abstract void sort();
+
+    public void analyze() {
+        // Analyze and print statistics
+        System.out.println("Iterations: " + iterations);
+        System.out.println("Comparisons: " + comparisons);
+        System.out.println("Swaps: " + swaps);
+    }
+}
