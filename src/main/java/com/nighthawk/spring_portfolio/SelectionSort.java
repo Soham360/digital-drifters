360991cc4d2fac6056fe0f5f2e62edfdb4da57d7
Aniket Chakradeo <91163802+AniCricKet@users.noreply.github.com>
2023-12-01 11:54:08 -0800

    Create SelectionSort.java

---
+++ src/main/java/com/nighthawk/spring_portfolio/SelectionSort.java
@@ -0,0 +1,30 @@
+public class SelectionSort extends Sort {
+
+    @Override
+    public void init(int[] data) {
+        this.data = data.clone();
+    }
+
+    @Override
+    public void sort() {
+        int n = data.length;
+
+        for (int i = 0; i < n-1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < n; j++) {
+                comparisons++;
+                if (data[j] < data[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap data[i] and data[minIndex]
+            int temp = data[minIndex];
+            data[minIndex] = data[i];
+            data[i] = temp;
+
+            swaps++;
+            iterations++;
+        }
+    }
+}
