80d82e5e4d3568bfd91cd545b0fefd1f1629c8d8
Aniket Chakradeo <91163802+AniCricKet@users.noreply.github.com>
2023-12-01 11:51:33 -0800

    Create BubbleSort.java

---
+++ src/main/java/com/nighthawk/spring_portfolio/BubbleSort.java
@@ -0,0 +1,28 @@
+public class BubbleSort extends Sort {
+
+    @Override
+    public void init(int[] data) {
+        this.data = data.clone();
+    }
+
+    @Override
+    public void sort() {
+        int n = data.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                comparisons++;
+
+                if (data[j] > data[j + 1]) {
+                    swaps++;
+
+                    // Swap data[j] and data[j+1]
+                    int temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+            iterations++;
+        }
+    }
+}
